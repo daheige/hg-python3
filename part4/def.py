7e175112f5a8149933297572e5f0bc171809f2d6
daheige <zhuwei313@hotmail.com>
2018-11-05 23:25:12 +0800

    函数基本使用

---
+++ part4/def.py
@@ -0,0 +1,134 @@
+def add_end(L=[]):
+    L.append('END')
+    return L
+
+print(add_end([1,2]))
+
+print(add_end())
+print(add_end()) #['END', 'END']
+
+'''很多初学者很疑惑，默认参数是[]，但是函数似乎每次都“记住了”上次添加了'END'后的list。
+
+原因解释如下：
+
+Python函数在定义的时候，默认参数L的值就被计算出来了，即[]，因为默认参数L也是一个变量，它指向对象[]，每次调用该函数，如果改变了L的内容，则下次调用时，默认参数的内容就变了，不再是函数定义时的[]了。
+'''
+'''
+定义默认参数要牢记一点：默认参数必须指向不变对象！
+'''
+
+def add_end2(L=None):
+    if L is None:
+        L = []
+    L.append('END')
+    return L
+
+print(add_end2([1,2,4]))
+print(add_end2())
+print(add_end2()) #['END']
+
+#可变参数 在参数的前面加上*
+def calc(*numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum + n * n
+    return sum
+
+print(calc(1,2,3))
+
+print(calc(*[1,2,3])) #Python允许你在list或tuple前面加一个*号，把list或tuple的元素变成可变参数传进去
+
+#关键字参数
+def person(name, age, **kw):
+    print('name:', name, 'age:', age, 'other:', kw)
+
+person(1,23,city="fefe") #name: 1 age: 23 other: {'city': 'fefe'} 把city转换为一个dict
+
+'''
+命名关键字参数
+对于关键字参数，函数的调用者可以传入任意不受限制的关键字参数。至于到底传入了哪些，就需要在函数内部通过kw检查。
+
+仍以person()函数为例，我们希望检查是否有city和job参数：
+
+def person(name, age, **kw):
+    if 'city' in kw:
+        # 有city参数
+        pass
+    if 'job' in kw:
+        # 有job参数
+        pass
+    print('name:', name, 'age:', age, 'other:', kw)
+但是调用者仍可以传入不受限制的关键字参数：
+
+>>> person('Jack', 24, city='Beijing', addr='Chaoyang', zipcode=123456)
+如果要限制关键字参数的名字，就可以用命名关键字参数，例如，只接收city和job作为关键字参数。这种方式定义的函数如下：
+
+def person(name, age, *, city, job):
+    print(name, age, city, job)
+和关键字参数**kw不同，命名关键字参数需要一个特殊分隔符*，*后面的参数被视为命名关键字参数。
+
+调用方式如下：
+
+>>> person('Jack', 24, city='Beijing', job='Engineer')
+Jack 24 Beijing Engineer
+如果函数定义中已经有了一个可变参数，后面跟着的命名关键字参数就不再需要一个特殊分隔符*了：
+
+def person(name, age, *args, city, job):
+    print(name, age, args, city, job)
+命名关键字参数必须传入参数名，这和位置参数不同。如果没有传入参数名，调用将报错：
+
+>>> person('Jack', 24, 'Beijing', 'Engineer')
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: person() takes 2 positional arguments but 4 were given
+由于调用时缺少参数名city和job，Python解释器把这4个参数均视为位置参数，但person()函数仅接受2个位置参数。
+
+命名关键字参数可以有缺省值，从而简化调用：
+
+def person(name, age, *, city='Beijing', job):
+    print(name, age, city, job)
+由于命名关键字参数city具有默认值，调用时，可不传入city参数：
+
+>>> person('Jack', 24, job='Engineer')
+Jack 24 Beijing Engineer
+使用命名关键字参数时，要特别注意，如果没有可变参数，就必须加一个*作为特殊分隔符。如果缺少*，Python解释器将无法识别位置参数和命名关键字参数：
+
+def person(name, age, city, job):
+    # 缺少 *，city和job被视为位置参数
+    pass
+参数组合
+在Python中定义函数，可以用必选参数、默认参数、可变参数、关键字参数和命名关键字参数，这5种参数都可以组合使用。但是请注意，参数定义的顺序必须是：必选参数、默认参数、可变参数、命名关键字参数和关键字参数。
+
+比如定义一个函数，包含上述若干种参数：
+
+def f1(a, b, c=0, *args, **kw):
+    print('a =', a, 'b =', b, 'c =', c, 'args =', args, 'kw =', kw)
+
+def f2(a, b, c=0, *, d, **kw):
+    print('a =', a, 'b =', b, 'c =', c, 'd =', d, 'kw =', kw)
+在函数调用的时候，Python解释器自动按照参数位置和参数名把对应的参数传进去。
+
+>>> f1(1, 2)
+a = 1 b = 2 c = 0 args = () kw = {}
+>>> f1(1, 2, c=3)
+a = 1 b = 2 c = 3 args = () kw = {}
+>>> f1(1, 2, 3, 'a', 'b')
+a = 1 b = 2 c = 3 args = ('a', 'b') kw = {}
+>>> f1(1, 2, 3, 'a', 'b', x=99)
+a = 1 b = 2 c = 3 args = ('a', 'b') kw = {'x': 99}
+>>> f2(1, 2, d=99, ext=None)
+a = 1 b = 2 c = 0 d = 99 kw = {'ext': None}
+最神奇的是通过一个tuple和dict，你也可以调用上述函数：
+
+>>> args = (1, 2, 3, 4)
+>>> kw = {'d': 99, 'x': '#'}
+>>> f1(*args, **kw)
+a = 1 b = 2 c = 3 args = (4,) kw = {'d': 99, 'x': '#'}
+>>> args = (1, 2, 3)
+>>> kw = {'d': 88, 'x': '#'}
+>>> f2(*args, **kw)
+a = 1 b = 2 c = 3 d = 88 kw = {'x': '#'}
+所以，对于任意函数，都可以通过类似func(*args, **kw)的形式调用它，无论它的参数是如何定义的。
+
+ 虽然可以组合多达5种参数，但不要同时使用太多的组合，否则函数接口的可理解性很差。
+'''
---
+++ part4/duigui_def.py
@@ -0,0 +1,47 @@
+def fact(n):
+    if n==1:
+        return 1
+    return n * fact(n - 1)
+
+print(fact(12))
+
+'''
+要改成尾递归方式，需要多一点代码，主要是要把每一步的乘积传入到递归函数中：
+
+def fact(n):
+    return fact_iter(n, 1)
+
+def fact_iter(num, product):
+    if num == 1:
+        return product
+    return fact_iter(num - 1, num * product)
+可以看到，return fact_iter(num - 1, num * product)仅返回递归函数本身，num - 1和num * product在函数调用前就会被计算，不影响函数调用。
+
+fact(5)对应的fact_iter(5, 1)的调用如下：
+
+===> fact_iter(5, 1)
+===> fact_iter(4, 5)
+===> fact_iter(3, 20)
+===> fact_iter(2, 60)
+===> fact_iter(1, 120)
+===> 120
+尾递归调用时，如果做了优化，栈不会增长，因此，无论多少次调用也不会导致栈溢出。
+
+遗憾的是，大多数编程语言没有针对尾递归做优化，Python解释器也没有做优化，所以，即使把上面的fact(n)函数改成尾递归方式，也会导致栈溢出。
+
+小结
+使用递归函数的优点是逻辑简单清晰，缺点是过深的调用会导致栈溢出。
+
+针对尾递归优化的语言可以通过尾递归防止栈溢出。尾递归事实上和循环是等价的，没有循环语句的编程语言只能通过尾递归实现循环。
+
+Python标准的解释器没有针对尾递归做优化，任何递归函数都存在栈溢出的问题。
+'''
+def fact1(n):
+    return fact_iter(n, 1)
+
+def fact_iter(num, product):
+    if num == 1:
+        return product
+    return fact_iter(num - 1, num * product)
+
+print(fact1(12))
